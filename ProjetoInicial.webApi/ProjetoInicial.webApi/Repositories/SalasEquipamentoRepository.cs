c6781712b54b65b18a0e8ce60c2e1db988e7e61e
FIC <FIC@info.local>
2021-08-04 11:11:47 -0300

    BD e API quase finalizados

---
+++ ProjetoInicial.webApi/ProjetoInicial.webApi/Repositories/SalasEquipamentoRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoInicial.webApi.Domains;
+using ProjetoInicial.webApi.Interfaces;
+using ProjetoInicial.webApi.Contexts;
+
+namespace ProjetoInicial.webApi.Repositories
+{
+    public class SalasEquipamentoRepository : ISalasEquipamento
+    {
+
+        primeiroProjetoContext ctx = new primeiroProjetoContext();
+
+        public void Atualizar(int id, SalasEquipamento salaEquipamentoAtualizada)
+        {
+            SalasEquipamento salaEquipamentoBuscado = ctx.SalasEquipamentos.Find(id);
+
+            if(salaEquipamentoBuscado.IdSala != 0 || salaEquipamentoBuscado.IdEquipamento != 0)
+            {
+                salaEquipamentoBuscado.IdSala = salaEquipamentoAtualizada.IdSala;
+                salaEquipamentoBuscado.IdEquipamento = salaEquipamentoAtualizada.IdEquipamento;
+            }
+
+            ctx.SalasEquipamentos.Update(salaEquipamentoBuscado);
+
+            ctx.SaveChanges();
+        }
+
+        public void Cadastrar(SalasEquipamento novaSalaEquipamento)
+        {
+            ctx.SalasEquipamentos.Add(novaSalaEquipamento);
+
+            ctx.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            SalasEquipamento salasEquipamentoBuscado = ctx.SalasEquipamentos.Find(id);
+
+            if(salasEquipamentoBuscado != null)
+            {
+                ctx.SalasEquipamentos.Remove(salasEquipamentoBuscado);
+
+                ctx.SaveChanges();
+            }
+
+            
+        }
+
+        public List<SalasEquipamento> ListarTodos()
+        {
+           return ctx.SalasEquipamentos.ToList();
+        }
+    }
+}
